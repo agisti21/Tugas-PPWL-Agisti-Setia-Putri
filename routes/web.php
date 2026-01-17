c4994194fb513a8e7fcc60438a3b900b71dabcc1
Agisti Setia Putri <gistisp@gmail.com>
2026-01-17 22:27:33 +0700

    Add login multi user

--- routes/web.php
+++ routes/web.php
@@ -9,17 +9,17 @@ use App\Models\Product;
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'verified', 'admin'])->name('dashboard');
+
+ Route::get('/', function () {
+        $products = Product::latest()->take(8)->get();
+        return view('user.home', compact('products'));
+    })->name('home');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
-    Route::get('/', function () {
-        $products = Product::latest()->take(8)->get();
-        return view('user.home', compact('products'));
-    })->name('home');
 
     Route::get('/admin', function () {
         return view('home');
